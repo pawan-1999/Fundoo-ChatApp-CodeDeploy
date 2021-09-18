0a022f996be8aa9a6cd2f464416c51b4259ea4e0
pawan-1999 <guptapawan767@gmail.com>
2021-09-18 07:30:56 +0000

    Fundoo-ChatApp-Deployment

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -102,12 +102,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbname',
-        'USER': 'user',
-        'PASSWORD': 'dsfdsfbdspfus',
-        'HOST': 'endpoint.google.com',
-        'PORT': '1234',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Pawan',
+        'USER': 'admin',
+        'PASSWORD': 'PawanGupta',
+        'HOST': 'chatappdb.c9fxbxtzo7p8.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 #sagar kadam
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source home/ubuntu/new_chatapp/venv/bin/activate
+cd /home/ubuntu/new_chatapp/fundoo/fundooapp/templates
